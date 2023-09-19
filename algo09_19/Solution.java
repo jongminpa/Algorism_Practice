ab16a95920ca3935821e6cd1d59447ec5b10e8cc
jongminpa <126760210+jongminpa@users.noreply.github.com>
2023-09-19 17:30:11 +0900

    Add files via upload

---
+++ algo09_19/Solution.java
@@ -0,0 +1,5 @@
+package algo09_19;
+
+public class Solution {
+
+}
---
+++ algo09_19/Solution1.java
@@ -0,0 +1,9 @@
+package algo09_19;
+
+public class Solution1 {
+	public int solution(int n){
+		String str = Integer.toString(n,3);
+		String reversed = new StringBuilder(str).reverse().toString();
+		return Integer.valueOf(reversed,3);
+	}
+}
